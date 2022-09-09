894569a00edd689887cbb6cc77c765051a207ebe
Arrafifajri <112527317+Arrafifajri@users.noreply.github.com>
2022-09-09 14:36:42 +0700

    Add files via upload

---
+++ UPRAK_Fajri_10TKJ4.py
@@ -0,0 +1,35 @@
+#soal satu
+#balok
+lb= 10
+pb= 15
+tb= 5
+
+balok= lb*pb*tb
+print("balok =15 * 10 * 5 =", balok)
+
+#kubus
+s = 150
+
+kubus = s * s * s
+print("kubus = 150 * 150 * 150 =", kubus)
+
+#limas
+al = 35
+tl = 29
+
+limas = 1/3 * 35 * 29
+print("limas = 1/3 * 35 * 29 =", limas)
+
+#tabung 
+r= 40
+t = 50
+tabung = 22/7 * r * r * t
+print(" tabung = 22/7 * 4 * 4 * 9 =", tabung)
+
+# scelcius ke reamur
+
+print ("Celcius to Reamur ")
+celcius =int(input("celcius = "))
+
+reamur= celcius  * 4/51
+print ("reamur =", reamur)
